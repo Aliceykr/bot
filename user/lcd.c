47d380e2d1f24537d29c2ccff16379fd7d1afdb9
Aliceykr <k1350489482@163.com>
2026-03-21 16:41:17 +0800

    换成了硬件spi，提高了lcd刷新速度

--- user/lcd.c
+++ user/lcd.c
@@ -1,66 +1,80 @@
 #include "lcd.h"
 #include "lcdfont.h"
 
+static spi_device_handle_t s_spi = NULL;
 static void lcd_delay_ms(uint32_t ms) { vTaskDelay(pdMS_TO_TICKS(ms)); }
+
+static void lcd_spi_send(const uint8_t *data, size_t len)
+{
+    if (len == 0) return;
+    spi_transaction_t t = { .length = len * 8, .tx_buffer = data };
+    spi_device_polling_transmit(s_spi, &t);
+}
 
 void LCD_GPIO_Init(void)
 {
+    // DC, BLK, RES 为普通 GPIO
     gpio_config_t io_conf = {};
     io_conf.intr_type    = GPIO_INTR_DISABLE;
     io_conf.mode         = GPIO_MODE_OUTPUT;
-    io_conf.pin_bit_mask = (1ULL<<LCD_MOSI_PIN)|(1ULL<<LCD_SCLK_PIN)|
-                           (1ULL<<LCD_RES_PIN) |(1ULL<<LCD_DC_PIN)  |
-                           (1ULL<<LCD_BLK_PIN);
-    io_conf.pull_down_en = 0;
-    io_conf.pull_up_en   = 0;
+    io_conf.pin_bit_mask = (1ULL<<LCD_RES_PIN)|(1ULL<<LCD_DC_PIN)|(1ULL<<LCD_BLK_PIN);
+    io_conf.pull_down_en = 0; io_conf.pull_up_en = 0;
     gpio_config(&io_conf);
-    gpio_set_level(LCD_MOSI_PIN, 1);
-    gpio_set_level(LCD_SCLK_PIN, 1);
-    gpio_set_level(LCD_RES_PIN,  1);
-    gpio_set_level(LCD_DC_PIN,   1);
-    gpio_set_level(LCD_BLK_PIN,  1);
+    gpio_set_level(LCD_RES_PIN, 1);
+    gpio_set_level(LCD_DC_PIN,  1);
+    gpio_set_level(LCD_BLK_PIN, 1);
+
+    // 初始化 SPI2 总线（无 CS，无 MISO）
+    spi_bus_config_t buscfg = {
+        .mosi_io_num   = LCD_MOSI_PIN,
+        .miso_io_num   = -1,
+        .sclk_io_num   = LCD_SCLK_PIN,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+        .max_transfer_sz = LCD_W * LCD_H * 2 + 8,
+    };
+    spi_bus_initialize(SPI2_HOST, &buscfg, SPI_DMA_CH_AUTO);
+
+    spi_device_interface_config_t devcfg = {
+        .clock_speed_hz = 40 * 1000 * 1000,
+        .mode           = 0,
+        .spics_io_num   = -1,  // 无 CS
+        .queue_size     = 7,
+    };
+    spi_bus_add_device(SPI2_HOST, &devcfg, &s_spi);
 }
 
-void LCD_Writ_Bus(uint8_t dat)
+void LCD_Writ_Bus(uint8_t dat) { lcd_spi_send(&dat, 1); }
+void LCD_WR_DATA8(uint8_t dat) { lcd_spi_send(&dat, 1); }
+void LCD_WR_DATA(uint16_t dat)
 {
-    uint8_t i;
-    for (i = 0; i < 8; i++) {
-        LCD_SCLK_Clr();
-        if (dat & 0x80) LCD_MOSI_Set(); else LCD_MOSI_Clr();
-        LCD_SCLK_Set();
-        dat <<= 1;
-    }
+    uint8_t buf[2] = {dat >> 8, dat & 0xFF};
+    lcd_spi_send(buf, 2);
 }
-
-void LCD_WR_DATA8(uint8_t dat)  { LCD_Writ_Bus(dat); }
-void LCD_WR_DATA(uint16_t dat)  { LCD_Writ_Bus(dat>>8); LCD_Writ_Bus(dat); }
-void LCD_WR_REG(uint8_t dat)    { LCD_DC_Clr(); LCD_Writ_Bus(dat); LCD_DC_Set(); }
+void LCD_WR_REG(uint8_t dat)
+{
+    gpio_set_level(LCD_DC_PIN, 0);
+    lcd_spi_send(&dat, 1);
+    gpio_set_level(LCD_DC_PIN, 1);
+}
 
 void LCD_Address_Set(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2)
 {
     LCD_WR_REG(0x2a);
-    LCD_WR_DATA(x1);
-    LCD_WR_DATA(x2);
+    LCD_WR_DATA(x1); LCD_WR_DATA(x2);
     LCD_WR_REG(0x2b);
-    LCD_WR_DATA(y1);
-    LCD_WR_DATA(y2);
+    LCD_WR_DATA(y1); LCD_WR_DATA(y2);
     LCD_WR_REG(0x2c);
 }
 
 void LCD_Init(void)
 {
     LCD_GPIO_Init();
+    LCD_RES_Clr(); lcd_delay_ms(100);
+    LCD_RES_Set(); lcd_delay_ms(100);
+    LCD_BLK_Set(); lcd_delay_ms(100);
 
-    LCD_RES_Clr();
-    lcd_delay_ms(100);
-    LCD_RES_Set();
-    lcd_delay_ms(100);
-    LCD_BLK_Set();
-    lcd_delay_ms(100);
-
-    LCD_WR_REG(0x11); // Sleep out
-    lcd_delay_ms(120);
-
+    LCD_WR_REG(0x11); lcd_delay_ms(120);
     LCD_WR_REG(0xCF);
     LCD_WR_DATA8(0x00); LCD_WR_DATA8(0xC1); LCD_WR_DATA8(0x30);
     LCD_WR_REG(0xED);
@@ -69,35 +83,23 @@ void LCD_Init(void)
     LCD_WR_DATA8(0x85); LCD_WR_DATA8(0x00); LCD_WR_DATA8(0x79);
     LCD_WR_REG(0xCB);
     LCD_WR_DATA8(0x39); LCD_WR_DATA8(0x2C); LCD_WR_DATA8(0x00); LCD_WR_DATA8(0x34); LCD_WR_DATA8(0x02);
-    LCD_WR_REG(0xF7);
-    LCD_WR_DATA8(0x20);
-    LCD_WR_REG(0xEA);
-    LCD_WR_DATA8(0x00); LCD_WR_DATA8(0x00);
-    LCD_WR_REG(0xC0);
-    LCD_WR_DATA8(0x1D);
-    LCD_WR_REG(0xC1);
-    LCD_WR_DATA8(0x12);
-    LCD_WR_REG(0xC5);
-    LCD_WR_DATA8(0x33); LCD_WR_DATA8(0x3F);
-    LCD_WR_REG(0xC7);
-    LCD_WR_DATA8(0x92);
-    LCD_WR_REG(0x3A);
-    LCD_WR_DATA8(0x55);
+    LCD_WR_REG(0xF7); LCD_WR_DATA8(0x20);
+    LCD_WR_REG(0xEA); LCD_WR_DATA8(0x00); LCD_WR_DATA8(0x00);
+    LCD_WR_REG(0xC0); LCD_WR_DATA8(0x1D);
+    LCD_WR_REG(0xC1); LCD_WR_DATA8(0x12);
+    LCD_WR_REG(0xC5); LCD_WR_DATA8(0x33); LCD_WR_DATA8(0x3F);
+    LCD_WR_REG(0xC7); LCD_WR_DATA8(0x92);
+    LCD_WR_REG(0x3A); LCD_WR_DATA8(0x55);
     LCD_WR_REG(0x36);
-    if      (USE_HORIZONTAL == 0) LCD_WR_DATA8(0x08);
-    else if (USE_HORIZONTAL == 1) LCD_WR_DATA8(0xC8);
-    else if (USE_HORIZONTAL == 2) LCD_WR_DATA8(0x78);
-    else                          LCD_WR_DATA8(0xA8);
-    LCD_WR_REG(0xB1);
-    LCD_WR_DATA8(0x00); LCD_WR_DATA8(0x12);
-    LCD_WR_REG(0xB6);
-    LCD_WR_DATA8(0x0A); LCD_WR_DATA8(0xA2);
-    LCD_WR_REG(0x44);
-    LCD_WR_DATA8(0x02);
-    LCD_WR_REG(0xF2);
-    LCD_WR_DATA8(0x00);
-    LCD_WR_REG(0x26);
-    LCD_WR_DATA8(0x01);
+    if(USE_HORIZONTAL==0)LCD_WR_DATA8(0x08);
+    else if(USE_HORIZONTAL==1)LCD_WR_DATA8(0xC8);
+    else if(USE_HORIZONTAL==2)LCD_WR_DATA8(0x78);
+    else LCD_WR_DATA8(0xA8);
+    LCD_WR_REG(0xB1); LCD_WR_DATA8(0x00); LCD_WR_DATA8(0x12);
+    LCD_WR_REG(0xB6); LCD_WR_DATA8(0x0A); LCD_WR_DATA8(0xA2);
+    LCD_WR_REG(0x44); LCD_WR_DATA8(0x02);
+    LCD_WR_REG(0xF2); LCD_WR_DATA8(0x00);
+    LCD_WR_REG(0x26); LCD_WR_DATA8(0x01);
     LCD_WR_REG(0xE0);
     LCD_WR_DATA8(0x0F); LCD_WR_DATA8(0x22); LCD_WR_DATA8(0x1C); LCD_WR_DATA8(0x1B);
     LCD_WR_DATA8(0x08); LCD_WR_DATA8(0x0F); LCD_WR_DATA8(0x48); LCD_WR_DATA8(0xB8);
@@ -108,7 +110,7 @@ void LCD_Init(void)
     LCD_WR_DATA8(0x10); LCD_WR_DATA8(0x07); LCD_WR_DATA8(0x38); LCD_WR_DATA8(0x47);
     LCD_WR_DATA8(0x4B); LCD_WR_DATA8(0x0A); LCD_WR_DATA8(0x13); LCD_WR_DATA8(0x06);
     LCD_WR_DATA8(0x30); LCD_WR_DATA8(0x38); LCD_WR_DATA8(0x0F);
-    LCD_WR_REG(0x29); // Display on
+    LCD_WR_REG(0x29);
 
     LCD_Fill(0, 0, LCD_W, LCD_H, RED);
 }
@@ -119,11 +121,20 @@ void LCD_Init(void)
 
 void LCD_Fill(uint16_t xsta, uint16_t ysta, uint16_t xend, uint16_t yend, uint16_t color)
 {
-    uint16_t i, j;
+    uint8_t color_h = color >> 8;
+    uint8_t color_l = color & 0xFF;
+    static uint8_t fill_buf[LCD_W * 2];  // 一行缓冲
+    uint32_t line_len = (xend - xsta) * 2;
+    for (uint32_t i = 0; i < line_len; i += 2) {
+        fill_buf[i]   = color_h;
+        fill_buf[i+1] = color_l;
+    }
     LCD_Address_Set(xsta, ysta, xend-1, yend-1);
-    for (i = ysta; i < yend; i++)
-        for (j = xsta; j < xend; j++)
-            LCD_WR_DATA(color);
+    gpio_set_level(LCD_DC_PIN, 1);
+    for (uint16_t i = ysta; i < yend; i++) {
+        spi_transaction_t t = { .length = line_len * 8, .tx_buffer = fill_buf };
+        spi_device_polling_transmit(s_spi, &t);
+    }
 }
 
 void LCD_DrawPoint(uint16_t x, uint16_t y, uint16_t color)
