118a3b5fec05a9d3ba2ea334bde773eff848f385
Aliceykr <k1350489482@163.com>
2026-03-21 14:10:06 +0800

    修复了一些bug

--- user/lcd.c
+++ user/lcd.c
@@ -158,7 +158,7 @@ void LCD_ShowChar(uint16_t x,uint16_t y,uint8_t num,uint16_t fc,uint16_t bc,uint
 }
 
 void LCD_ShowString(uint16_t x,uint16_t y,const uint8_t *p,uint16_t fc,uint16_t bc,uint8_t sizey,uint8_t mode)
-{ while(*p!='\''\0'\''){LCD_ShowChar(x,y,*p,fc,bc,sizey,mode);x+=sizey/2;p++;} }
+{ while(*p != '\0'){LCD_ShowChar(x,y,*p,fc,bc,sizey,mode);x+=sizey/2;p++;} }
 
 uint32_t mypow(uint8_t m,uint8_t n){ uint32_t r=1; while(n--)r*=m; return r; }
 
